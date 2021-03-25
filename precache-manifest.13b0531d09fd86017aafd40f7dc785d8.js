ec2851b7b69384bd924422bbe694e282f25ec885
heinhtet naing <6927125-hhn186@users.noreply.gitlab.com>
2021-03-25 18:15:43 +0630

    Updates

---
+++ precache-manifest.13b0531d09fd86017aafd40f7dc785d8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187f4d8864ce496f3a5fc70bd5662397",
+    "url": "/tyrantinc/index.html"
+  },
+  {
+    "revision": "fd2e0608050dd4d1e202",
+    "url": "/tyrantinc/static/css/2.232116d9.chunk.css"
+  },
+  {
+    "revision": "bc12b427f3732bc46dea",
+    "url": "/tyrantinc/static/css/main.52af02f5.chunk.css"
+  },
+  {
+    "revision": "fd2e0608050dd4d1e202",
+    "url": "/tyrantinc/static/js/2.f238a04d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tyrantinc/static/js/2.f238a04d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc12b427f3732bc46dea",
+    "url": "/tyrantinc/static/js/main.4d07d0b6.chunk.js"
+  },
+  {
+    "revision": "cef8a01b46b323eee863",
+    "url": "/tyrantinc/static/js/runtime-main.31980948.js"
+  },
+  {
+    "revision": "dee51fde0cc660a2f2dcce2d4c03fd55",
+    "url": "/tyrantinc/static/media/cart.dee51fde.svg"
+  },
+  {
+    "revision": "04638ad444ba4063f4af9f9c23c865ea",
+    "url": "/tyrantinc/static/media/goh-rhy-yan-XgsVB9Uwpq8-unsplash.04638ad4.jpg"
+  },
+  {
+    "revision": "1f690fee39bcc99655c8a0049e2e994d",
+    "url": "/tyrantinc/static/media/marco-xu-x2tnHqvWYB0-unsplash.1f690fee.jpg"
+  },
+  {
+    "revision": "f5db9192644fe5a74485d64b384cb591",
+    "url": "/tyrantinc/static/media/mcjo-sy-mSOIqZ1Dlkw-unsplash.f5db9192.jpg"
+  },
+  {
+    "revision": "76e0f49c069616a041f4b507dea109a2",
+    "url": "/tyrantinc/static/media/search.76e0f49c.svg"
+  }
+]);
